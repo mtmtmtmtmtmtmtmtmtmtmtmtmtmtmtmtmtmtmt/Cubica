d4fd0ed2d9d8781a7bff349be57985c97f119011
Nyrose <rooty910@gmail.com>
2026-01-30 21:11:44 +0330

    moving to java 17

--- src/main/java/ir/mt/Main.java
+++ src/main/java/ir/mt/Main.java
@@ -1,11 +1,7 @@
 package ir.mt;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        System.out.println("Hello 1World!");
     }
 }
