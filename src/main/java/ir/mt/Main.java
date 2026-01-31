4e11f1c5611f5fc0b0e049b02a7ed71e584da4d5
Nyrose <rooty910@gmail.com>
2026-01-31 20:56:50 +0330

    Starting over

--- src/main/java/ir/mt/Main.java
+++ src/main/java/ir/mt/Main.java
@@ -1,22 +1,6 @@
 package ir.mt;
 
-import ir.mt.render.Renderer;
-import ir.mt.window.WindowManager;
-import static org.lwjgl.opengl.GL33.*; // for GL_TRIANGLE_STRIP
-
 public class Main {
     public static void main(String[] args) {
-
-        float[] squareVertices = {
-                -0.5f,  0.5f, 0.0f,  // top-left
-                -0.5f, -0.5f, 0.0f,  // bottom-left
-                0.5f,  0.5f, 0.0f,  // top-right
-                0.5f, -0.5f, 0.0f   // bottom-right
-        };
-
-        Renderer renderer = new Renderer();
-        WindowManager wm = new WindowManager(renderer, () -> squareVertices, GL_TRIANGLE_STRIP);
-
-        wm.run(); // WindowManager will call renderer.init() at the right time
     }
 }
